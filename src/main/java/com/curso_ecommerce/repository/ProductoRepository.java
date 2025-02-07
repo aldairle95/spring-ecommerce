7ad6eefb30c97727642fe1f3e7349061188a3d1b
aldairle95 <aldairleal254@gmail.com>
2025-02-07 16:56:13 -0500

    creacion de rpository y las clasesy el metodo show de producto controller

---
+++ src/main/java/com/curso_ecommerce/repository/ProductoRepository.java
@@ -0,0 +1,10 @@
+package com.curso_ecommerce.repository;
+
+import com.curso_ecommerce.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
+}
---
+++ src/main/java/com/curso_ecommerce/service/ProductoService.java
@@ -0,0 +1,13 @@
+package com.curso_ecommerce.service;
+
+import com.curso_ecommerce.model.Producto;
+
+import java.util.Optional;
+
+public interface ProductoService {
+
+    public Producto save(Producto producto);
+    public Optional<Producto> get(Integer id);
+    public void update(Producto producto);
+    public void delete(Integer id);
+}
