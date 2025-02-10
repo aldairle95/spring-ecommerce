7afa1647fcf3c560f03df35afeb8e9d70929e1c9
aldairle95 <aldairleal254@gmail.com>
2025-02-10 10:02:55 -0500

    funcionalidad mostrar productos vista show

--- src/main/java/com/curso_ecommerce/service/ProductoService.java
+++ src/main/java/com/curso_ecommerce/service/ProductoService.java
@@ -2,6 +2,7 @@ package com.curso_ecommerce.service;
 
 import com.curso_ecommerce.model.Producto;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductoService {
@@ -10,4 +11,5 @@ public interface ProductoService {
     public Optional<Producto> get(Integer id);
     public void update(Producto producto);
     public void delete(Integer id);
+    public List<Producto> findAll();
 }
