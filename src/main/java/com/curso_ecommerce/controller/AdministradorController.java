baf7e62c2d3ebec6536853b7f6213a70296cde62
aldairle95 <aldairleal254@gmail.com>
2025-02-13 10:49:56 -0500

    funcionalidad carga de imagenes y mostar imagenes en el home.html

--- src/main/java/com/curso_ecommerce/controller/AdministradorController.java
+++ src/main/java/com/curso_ecommerce/controller/AdministradorController.java
@@ -1,16 +1,27 @@
 package com.curso_ecommerce.controller;
 
+import com.curso_ecommerce.model.Producto;
+import com.curso_ecommerce.service.ProductoService;
 import jakarta.persistence.GeneratedValue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorController {
 
+    @Autowired
+    private ProductoService productoService;
+
     @GetMapping("")
-    public String home(){
+    public String home(Model model) {
+        List<Producto> productos= productoService.findAll();
+        model.addAttribute("productos",productos);
         return "administrador/home";
     }
 }
