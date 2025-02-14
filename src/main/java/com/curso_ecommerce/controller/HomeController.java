b9a8643bbeb726c4bf54e9bbee99895238551b08
aldairle95 <aldairleal254@gmail.com>
2025-02-14 11:20:38 -0500

    se creo la ruta en controlador homecontroler para ver el producto se pasaron los datos

--- src/main/java/com/curso_ecommerce/controller/HomeController.java
+++ src/main/java/com/curso_ecommerce/controller/HomeController.java
@@ -1,22 +1,42 @@
 package com.curso_ecommerce.controller;
 
 
+import com.curso_ecommerce.model.Producto;
 import com.curso_ecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
+    private final Logger log= LoggerFactory.getLogger(HomeController.class);
+
     @Autowired
     private ProductoService productoService;
+
     @GetMapping("")
     public String home(Model model){
         model.addAttribute("productos",productoService.findAll());
         return "usuario/home";
     }
+
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id, Model model){
+        log.info("variable enviado como parametro {}",id);
+        Producto producto= new Producto();
+        Optional<Producto> productoOptional= productoService.get(id);
+        producto= productoOptional.get();
+        model.addAttribute("producto",producto);
+        return "usuario/productohome";
+    }
+
 }
